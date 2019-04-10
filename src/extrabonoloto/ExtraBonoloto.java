93dabc86b6adf9db669685951445d4209528ff24
cromerofajar <cromerofajar@nautilus7.danielcastelao.org>
2019-04-10 11:17:26 +0200

    ExtraBonoloto

---
+++ src/extrabonoloto/ExtraBonoloto.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package extrabonoloto;
+
+/**
+ *
+ * @author cromerofajar
+ */
+public class ExtraBonoloto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        BonolotoPaleta bono=new BonolotoPaleta();
+        bono.setVisible(true);
+    }
+    
+}
